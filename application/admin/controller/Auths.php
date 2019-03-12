a706aa996f578fc2d7469e63b869a071090801f8
yxd917 <1292505152@qq.com>
2019-03-12 11:29:55 +0800

    add role

--- application/admin/controller/Auths.php
+++ application/admin/controller/Auths.php
@@ -6,24 +6,25 @@ use app\common\controller\Adminbase;
 
 class Auths extends Adminbase
 {
-	public function users(){
-		if(request()->isAjax()){
-            $val=input('val');
-            $url['val'] = $val;
-            $this->assign('testval',$val);
-            $map='';
-            if($val){
-                $map['username']= array('like',"%".$val."%");
-            }
-            if (session('aid')!=1){
-                $map='userid='.session('aid');
-            }
-            $list = model('admin')->select();
+	public function users(){		
+        return view();
+	}
+
+    public function userslist(){
+        if(request()->isPost()){
+            $params = request()->param();
+            $map=[];
+            $page = $params['page'];
+            $start = ($page-1)*$params['limit'];
+            if(isset($params['username'])){
+                $map[] = ['username','like',"%".$params['username']."%"];
+            }            
+            $list = model('admin')->where($map)->limit($start,$params['limit'])->select();
+            $count = model('admin')->where($map)->count();
             foreach ($list as $key => $value) {
                 $list[$key]['group'] = model('role')->where('id',$value['groupid'])->value('name');
             }
-            return $result = ['code'=>0,'msg'=>'获取成功!','data'=>$list,'rel'=>1];
+            return json(['code'=>0,'msg'=>'','count'=>$count,'data'=>$list]);
         }
-        return view();
-	}
+    }
 }
